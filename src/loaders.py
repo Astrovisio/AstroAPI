4e8321eadd64beaa31477b6bb6151aa9f970fb19
Tommaso Massaglia <tommaso.massaglia@alkemy.com>
2025-03-04 17:33:59 +0100

    Added basic functions to get keys and thresholds.

---
+++ src/loaders.py
@@ -0,0 +1,23 @@
+import pynbody
+from spectral_cube import SpectralCube
+from .utils import getFileType
+
+def loadSimulation(path:str) -> pynbody.snapshot:
+    
+    sim = pynbody.load(path)
+    
+    return sim
+
+def loadObservation(path:str) -> SpectralCube:
+    
+    obs = SpectralCube.read(path)
+    
+    return obs
+
+def load(path:str):
+    
+    if getFileType(path) == "fits":
+        return loadObservation(path)
+    
+    else:
+        return loadSimulation(path)
